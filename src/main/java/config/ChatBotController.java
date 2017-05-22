8f7cf304862c7cb4c4391924e4124522a69211df
priyanka897 <priyanka.mittal@maxlifeinsurance.com>
2017-05-22 13:00:37 +0530

    Update ChatBotController.java

--- src/main/java/config/ChatBotController.java
+++ src/main/java/config/ChatBotController.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import commons.Commons;
+import common.Commons;
 import handlerservice.MliDoc_Handler_Service;
 import handlerservice.OTP_Handler_Service;
 import handlerservice.PolicyDetail_Handler_Service;
