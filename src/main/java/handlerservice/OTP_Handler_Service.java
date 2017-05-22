f3208729a41822fa816423f4080a31091dca2f9f
priyanka897 <priyanka.mittal@maxlifeinsurance.com>
2017-05-22 12:23:22 +0530

    Update OTP_Handler_Service.java

--- src/main/java/handlerservice/OTP_Handler_Service.java
+++ src/main/java/handlerservice/OTP_Handler_Service.java
@@ -10,7 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import cacheservice.OTPService;
-import commons.Commons;
+import common.Commons;
 
 
 @Component
