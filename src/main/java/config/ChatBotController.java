78a3249b1e373f88da32e1767719ad5e48e7c2e1
priyanka897 <priyanka.mittal@maxlifeinsurance.com>
2017-05-23 14:35:26 +0530

    Update ChatBotController.java

--- src/main/java/config/ChatBotController.java
+++ src/main/java/config/ChatBotController.java
@@ -56,67 +56,286 @@ public class ChatBotController {
 			String policy_Number = object.getJSONObject("result").getJSONObject("parameters").getJSONObject("PolicyNumber").get("Given-PolicyNumber")+"";
 			logger.info("Request PolicyNo :- "+policy_Number );
 
-			if ("PolicyNumberValidation".equals(action))
+			switch(action)
 			{
-				logger.info("CameInsdie Action :: PolicyNumberValidation");
-				if (serviceResp.get("PolicyNo") != null && (serviceResp.get("ValidOTP") != null && serviceResp.get("ValidOTP") != "" ))
-				{
-					logger.info("Inside :: PolicyNo & OTP Validation From ehcache");
-					String policyNumberInSession = serviceResp.get("PolicyNo") ;
-					System.out.println("Policy number in session :- " + policyNumberInSession);
-					System.out.println("Current PolicyNo :-  " + policy_Number);
 
-					if (policyNumberInSession != null) 
+			case "OTP.NotAvailable":
+			{
+				if(responsecache_onsessionId.containsKey(sessionId))
+				{
+					String cachePolicyNo=responsecache_onsessionId.get(sessionId).get("PolicyNo")+"";
+					String cacheOTP=responsecache_onsessionId.get(sessionId).get("policyotp")+"";
+					String proposerName=responsecache_onsessionId.get(sessionId).get("proposerName")+"";
+					Map<String, Object> serviceResp = responsecache_onsessionId.get(sessionId);
+					serviceResp.remove("cachevalidOTP");
+					responsecache_onsessionId.put(sessionId, serviceResp);
+					Map<String, String> orignalData = oTP_Handler_Service.getPolicyOtp(policy_Number, sessionId , 0);
+					speech = orignalData.get("Message");
+					if (orignalData.get("policyotp") != null) {
+						orignalData.put(cacheOTP, orignalData.get("policyotp"));
+						orignalData.put(proposerName, orignalData.get("proposerName"));
+						orignalData.put(cachePolicyNo, cachePolicyNo);
+						responsecache_onsessionId.put(sessionId, orignalData);
+						System.out.println("OTP is **** " + serviceResp.get("policyotp"));
+					}
+				}
+				else{
+					speech = "Thank you for contacting Max Life. Have a great day!";
+				}
+			}
+			break;
+			case "PolicyNumberValidation":
+			{
+				if(responsecache_onsessionId.containsKey(sessionId))
+				{
+					String cachePolicyNo=responsecache_onsessionId.get(sessionId).get("PolicyNo")+"";
+					String cachevalidOTP=responsecache_onsessionId.get(sessionId).get("ValidOTP")+"";
+					String cacheOTP=responsecache_onsessionId.get(sessionId).get("policyotp")+"";
+					String proposerName=responsecache_onsessionId.get(sessionId).get("proposerName")+"";
+
+					logger.info("CameInsdie Action :: PolicyNumberValidation");
+					if (cachePolicyNo!= null && (cachevalidOTP != null && !cachevalidOTP.equalsIgnoreCase("")))
 					{
 						logger.info("Inside :: PolicyNumberInSession Check !=null");
-						if (!(policy_Number.equals(policyNumberInSession)))
+						if (!(policy_Number.equals(cachePolicyNo)))
 						{
 							System.out.println("A new policy number entered by the customer " + policy_Number);
 							logger.info("A new policy number entered by the customer " + policy_Number);
 						}
 					}
-				}
-				if (serviceResp.get("PolicyNo") != null && (serviceResp.get("ValidOTP") != null && serviceResp.get("ValidOTP") != "" ))
-				{
-					logger.info("Inside 2 Check :: PolicyNo & OTP Validation From ehcache");
-					speech = "OTP Verification is completed for Policy Number " + serviceResp.get("PolicyNo")
-					+ ", please tell what you want to know about policy";
-					logger.info("Message Sent to Api :-"+ speech);
-				}
-				else if (serviceResp.get("PolicyNo") != null) 
-				{
-					String otp_session = "";
-					String cache_otp = serviceResp.get("policyotp");
-					if (cache_otp != null)
+					if (cachePolicyNo!= null && (cachevalidOTP != null && !cachevalidOTP.equalsIgnoreCase("")))
 					{
-						otp_session = serviceResp.get("policyotp");
+						logger.info("Inside 2 Check :: PolicyNo & OTP Validation From ehcache");
+						speech = "OTP Verification is completed for Policy Number " + cachePolicyNo
+								+ ", please tell what you want to know about policy";
+						logger.info("Message Sent to Api :-"+ speech);
 					}
-					if (otp_session != null)
+					else if (cachePolicyNo!= null) 
 					{
-						if (otp_session.equals(policy_Number)) {
-							speech = "Hi " + Commons.toCamelCase(serviceResp.get("proposerName").toString())
-							+ resProp.getString("welcomeUser");
-							Map data = policyInfo_Handler_Service.getPolicyInfo(serviceResp.get("PolicyNo").toString());
-							System.out.println("data----------" + data.toString());
-							serviceResp.put("ValidOTP", serviceResp.get("PolicyNo"));
-							serviceResp.remove("policyotp");
-							
-						} else {
-							speech = resProp.getString("OTPnotmatch");
+						String otp_session="";
+						if (!"".equalsIgnoreCase(cacheOTP) && cacheOTP != null)
+						{
+							otp_session = cacheOTP;
 						}
-					} else {
-						speech = resProp.getString("GenerateOTP");
+						if (!"".equalsIgnoreCase(otp_session) &&  otp_session != null)
+						{
+							policy_Number="123456";
+							if (otp_session.equals(policy_Number)) {
+								speech = "Hi " + Commons.toCamelCase(proposerName)
+								+ resProp.getString("welcomeUser");
+								Map data = policyInfo_Handler_Service.getPolicyInfo(cachePolicyNo);
+								System.out.println("data----------" + data.toString());
+								Map<String, Object> serviceResp = responsecache_onsessionId.get(sessionId);
+								serviceResp.put("ValidOTP", cachePolicyNo);
+								serviceResp.put("PolData", data);
+								serviceResp.remove("policyotp");
+								responsecache_onsessionId.put(sessionId, serviceResp);
+							} else {
+								speech = resProp.getString("OTPnotmatch");
+							}
+						} 
+						else
+						{
+							speech = resProp.getString("GenerateOTP");
+						}
 					}
 				}
 				else
 				{
 					logger.info("START :: Request For Generate PolicyOTP");
-					serviceResp = oTP_Handler_Service.getPolicyOtp(policy_Number, sessionId , 0);
+					Map<String, String> orignalData = oTP_Handler_Service.getPolicyOtp(policy_Number, sessionId , 0);
+					responsecache_onsessionId.put(sessionId, orignalData);
 					logger.info("END :: Request For Generate PolicyOTP :- "+serviceResp);
-					speech = serviceResp.get("Message");
+					speech = orignalData.get("Message");
 				}
 			}
-		} catch (Exception e) {
+			break;
+			case "OTPValidation":
+			{
+				if(responsecache_onsessionId.containsKey(sessionId))
+				{
+					String cachePolicyNo=responsecache_onsessionId.get(sessionId).get("PolicyNo")+"";
+					String cachevalidOTP=responsecache_onsessionId.get(sessionId).get("ValidOTP")+"";
+					String cacheOTP=responsecache_onsessionId.get(sessionId).get("policyotp")+"";
+					if ("".equalsIgnoreCase(cachePolicyNo)||cachePolicyNo == null)
+					{
+						speech = resProp.getString("validPolicyMessage");
+					}
+					else if (!"".equalsIgnoreCase(cachevalidOTP) && cachevalidOTP!= null) 
+					{
+						speech = "OTP Verification is completed for Policy Number "+ cachePolicyNo
+								+ ", please tell what you want to know about policy  OR write reset to start new session";
+					} 
+					else
+					{
+						String otp_session = "";
+						String OTP_request=object.getJSONObject("result").getJSONObject("parameters").getJSONObject("OTP").get("Provided-OTP")+"";
+						//						String requiredadata = object.getJSONObject("result").getJSONArray("contexts").getJSONObject(0)
+						//								.getJSONObject("parameters").getJSONObject("PolicyNumber").get("Given-PolicyNumber.original") + "";
+						otp_session = cacheOTP;
+						if (!"".equalsIgnoreCase(otp_session) && otp_session != null) {
+							if (otp_session.equals(OTP_request)) 
+							{
+								speech = "Mr. Arun. What information you want to know about your policy";
+								Map data = policyInfo_Handler_Service.getPolicyInfo(cachePolicyNo);
+								System.out.println("data----------" + data.toString());
+
+								Map<String, String> serviceResp = responsecache_onsessionId.get(sessionId);
+								serviceResp.put("cachevalidOTP", OTP_request);
+								serviceResp.remove("policyotp");
+								responsecache_onsessionId.put(sessionId, serviceResp);
+							} 
+							else {
+								speech = "OTP did not match.Please provide correct OTP.";
+							}
+						} else
+						{
+							speech = "You have not generated OTP.Please provide valid policy to generate OTP";
+						}
+					}
+				} 
+				else{
+					speech = "Thank you for contacting Max Life. Have a great day!";
+				}
+			}
+			break;
+			case "input.CTP":
+			{
+				if(responsecache_onsessionId.containsKey(sessionId))
+				{
+					String cachePolicyNo=responsecache_onsessionId.get(sessionId).get("PolicyNo")+"";
+					String cachevalidOTP=responsecache_onsessionId.get(sessionId).get("ValidOTP")+"";
+					Map data =(Map)responsecache_onsessionId.get(sessionId).get("PolData");
+					if ("".equalsIgnoreCase(cachePolicyNo) || cachePolicyNo==null )
+					{
+						speech = resProp.getString("validPolicyMessage");
+					} else if ("".equalsIgnoreCase(cachevalidOTP) || cachevalidOTP== null) 
+					{
+						speech = resProp.getString("validateOTP").concat(cachePolicyNo);
+
+					} else {
+						System.out.println("i am in ctp action 1");
+						if (data.get("CTP") == null) 
+						{
+							speech = ((Map) data.get("ErrorMessage")).get("Message").toString();
+						} else 
+						{
+							System.out.println("i am in ctp action 3");
+							Map ctp = (Map) data.get("CTP");
+							speech = ctp.get("Message").toString();
+						}
+					}
+				}
+				else{
+					speech = "Thank you for contacting Max Life. Have a great day!";
+				}
+			}
+			break;
+			case "input.Surrender": 
+			{
+				if(responsecache_onsessionId.containsKey(sessionId))
+				{
+					String cachePolicyNo=responsecache_onsessionId.get(sessionId).get("PolicyNo")+"";
+					String cachevalidOTP=responsecache_onsessionId.get(sessionId).get("ValidOTP")+"";
+					Map data =(Map)responsecache_onsessionId.get(sessionId).get("PolData");
+					if ("".equalsIgnoreCase(cachePolicyNo)||cachePolicyNo == null)
+					{
+						speech = resProp.getString("validPolicyMessage");
+					} 
+					else if ("".equalsIgnoreCase(cachevalidOTP)||cachevalidOTP== null)
+					{
+						speech = resProp.getString("validateOTP").concat(cachePolicyNo);
+					} 
+					else 
+					{
+						System.out.println("I am in Surrender Value");
+						if (data.get("CSV") == null) {
+							System.out.println("CSV is null is policy data");
+							speech = policyDetail_Handler_Service.getPolicyDetails(data, cachePolicyNo).get("Message");
+							System.out.println("Speech is " + speech);
+						}
+						else{
+							speech = ((Map)data.get("CSV")).get("Message").toString();
+						}
+					}
+				}
+				else{
+					speech = "Thank you for contacting Max Life. Have a great day!";
+				}
+			}
+			break;
+			case "input.Receipt":
+			{
+				if(responsecache_onsessionId.containsKey(sessionId))
+				{
+					String cachePolicyNo=responsecache_onsessionId.get(sessionId).get("PolicyNo")+"";
+					String cachevalidOTP=responsecache_onsessionId.get(sessionId).get("ValidOTP")+"";
+					Map data =(Map)responsecache_onsessionId.get(sessionId).get("PolData");
+
+					if ("".equalsIgnoreCase(cachePolicyNo)|| cachePolicyNo == null) {
+						speech = resProp.getString("validPolicyMessage");
+					} else if ("".equalsIgnoreCase(cachevalidOTP) || cachevalidOTP == null) {
+						speech = resProp.getString("validateOTP").concat(cachePolicyNo);
+					} else {
+						speech = mliDoc_Handler_Service.getMliDocService(cachePolicyNo).get("Message");
+					}
+				}
+				else{
+					speech = "Thank you for contacting Max Life. Have a great day!";
+				}
+			}
+			break;
+			case "input.Fund": 
+			{
+				if(responsecache_onsessionId.containsKey(sessionId))
+				{
+					String cachePolicyNo=responsecache_onsessionId.get(sessionId).get("PolicyNo")+"";
+					String cachevalidOTP=responsecache_onsessionId.get(sessionId).get("ValidOTP")+"";
+					Map data =(Map)responsecache_onsessionId.get(sessionId).get("PolData");
+					if ("".equalsIgnoreCase(cachePolicyNo) || cachePolicyNo == null) {
+						speech = resProp.getString("validPolicyMessage");
+					} else if ("".equalsIgnoreCase(cachevalidOTP) || cachevalidOTP == null) {
+						speech = resProp.getString("validateOTP").concat(cachePolicyNo);
+					} else {
+						if (data.get("FV") == null) {
+							speech = ((Map) data.get("ErrorMessage")).get("Message").toString();
+						} else {
+							Map fv = (Map) data.get("FV");
+							if (fv.get("fundValAsonDate") != null) {
+
+								if (fv.get("FVErrorMessage") != null)
+									speech = fv.get("FVErrorMessage").toString();
+								else {
+									String fvdata = fv.get("fundValAsonDate").toString();
+									speech = fv.get("Message").toString() + Math.round(Double.parseDouble(fvdata)*100.0)/100.0;
+								}
+							} else {
+								speech = fv.get("Message").toString();
+							}
+						}
+					}
+				} 
+				else{
+					speech = "Thank you for contacting Max Life. Have a great day!";
+				}
+			}
+			break;
+			case "close.conversation":
+			{
+				if(responsecache_onsessionId.containsKey(sessionId))
+				{
+					responsecache_onsessionId.clear();
+					speech = "Thank you for contacting Max Life. Have a great day!";
+				}
+				else{
+					speech = "Thank you for contacting Max Life. Have a great day!";
+				}
+			}
+			default : 
+				speech = "Thank you for contacting Max Life. Have a great day!";
+			}
+		} 
+		catch (Exception e) {
 			e.printStackTrace();
 		}
 		System.out.println(speech);
