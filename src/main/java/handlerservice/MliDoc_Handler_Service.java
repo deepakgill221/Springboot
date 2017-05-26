4e049acb1769fa35b733a5d4271a678dc062a601
priyanka897 <priyanka.mittal@maxlifeinsurance.com>
2017-05-26 15:19:02 +0530

    Update MliDoc_Handler_Service.java

--- src/main/java/handlerservice/MliDoc_Handler_Service.java
+++ src/main/java/handlerservice/MliDoc_Handler_Service.java
@@ -1,22 +1,17 @@
 package handlerservice;
-
 import java.util.HashMap;
 import java.util.Map;
 import java.util.ResourceBundle;
-
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.stereotype.Component;
-
 import common.Commons;
 import common.Httpurl_Connection;
-
-
 
 @Component
 public class MliDoc_Handler_Service 
 {
-	private static Logger logger = LogManager.getLogger(MliDoc_Handler_Service.class);
+		private static Logger logger = LogManager.getLogger(MliDoc_Handler_Service.class);
 	public static ResourceBundle res = ResourceBundle.getBundle("errorMessages");
 	public Map<String, String> getMliDocService(String policyNo) 
 	{
@@ -28,9 +23,11 @@ public class MliDoc_Handler_Service
 		logger.info("Result Get From HttpUrlConnection :- "+ result.toString());
 		try
 		{
-			if(!result.equalsIgnoreCase("InvalidResponse"))
+			Map resultData = Commons.getGsonData(result);
+			String soaStatusCode = ((Map) ((Map) resultData.get("response")).get("responseData"))
+					.get("soaStatusCode").toString();
+			if (soaStatusCode != null && !"".equalsIgnoreCase(soaStatusCode) && soaStatusCode.equalsIgnoreCase("200"))
 			{
-				Map resultData = Commons.getGsonData(result);
 				logger.info("Result Data :-"+ resultData );
 				String responseDescription = ((Map) ((Map) ((Map) resultData.get("response")).get("responseData"))
 						.get("dispatchDocumentsResponse")).get("responseDescription").toString();
