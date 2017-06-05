60bf7973ba5f10898c6684bb8c90b02081aaba63
priyanka897 <priyanka.mittal@maxlifeinsurance.com>
2017-06-05 17:12:54 +0530

    Update OTPServiceimpl.java

--- src/main/java/cacheservice/OTPServiceimpl.java
+++ src/main/java/cacheservice/OTPServiceimpl.java
@@ -16,11 +16,11 @@ public class OTPServiceimpl implements OTPService
 {
 	private static Logger logger = LogManager.getLogger(OTPServiceimpl.class);
 	@Cacheable(value="chatbot", key="#sessionId.concat('-').concat(#policyNo)", unless="#result == null")
-	public String OTPCallCashing(String policyNo,String sessionId, String identifier) throws SQLException , Exception
+	public String OTPCallCashing(String policyNo,String sessionId, String identifier , String finaldate) throws SQLException , Exception
 	{
 		logger.info("CameInside Method :: OTPCallCashing ");
 		String finalresult="";
 		Httpurl_Connection connection = new Httpurl_Connection();
-		return  finalresult = connection.httpConnection_response(policyNo, identifier);
+		return  finalresult = connection.httpConnection_response(policyNo, identifier, finaldate);
 	}
 }
