0d872462c33571c83e315c82987058819f9c5f9c
priyanka897 <priyanka.mittal@maxlifeinsurance.com>
2017-06-05 17:34:42 +0530

    Update MliDoc_Handler_Service.java

--- src/main/java/handlerservice/MliDoc_Handler_Service.java
+++ src/main/java/handlerservice/MliDoc_Handler_Service.java
@@ -6,7 +6,7 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.stereotype.Component;
 import common.Commons;
-import common.CustomizeDate
+import common.CustomizeDate;
 import common.Httpurl_Connection;
 
 @Component
