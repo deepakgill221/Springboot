eb79ce1b9492cc799f65131d57b9ae4a0c3a341b
priyanka897 <priyanka.mittal@maxlifeinsurance.com>
2017-06-05 16:53:51 +0530

    Update OTPService.java

--- src/main/java/cacheservice/OTPService.java
+++ src/main/java/cacheservice/OTPService.java
@@ -4,6 +4,6 @@ import java.sql.SQLException;
 
 public interface OTPService 
 {
-	public String OTPCallCashing(String policyNo,String sessionId, String identifier) throws SQLException , Exception;
+	public String OTPCallCashing(String policyNo,String sessionId, String identifier, String finalDate) throws SQLException , Exception;
 
 }
