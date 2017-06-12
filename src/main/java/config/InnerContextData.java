2a33b199c5972e613f49f1676735cee22ef981cf
priyanka897 <priyanka.mittal@maxlifeinsurance.com>
2017-06-12 13:20:23 +0530

    Add files via upload

---
+++ src/main/java/config/InnerContextData.java
@@ -0,0 +1,16 @@
+package com.qc.api.response;
+
+public class InnerContextData 
+{
+	private String givenPolicyNumber;
+
+
+	public String getGivenPolicyNumber() {
+		return givenPolicyNumber;
+	}
+
+	public void setGivenPolicyNumber(String givenPolicyNumber) {
+		this.givenPolicyNumber = givenPolicyNumber;
+	}
+	
+}
