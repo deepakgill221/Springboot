6175656981e2a96260db1c9c6f675c91fe4331f4
priyanka897 <priyanka.mittal@maxlifeinsurance.com>
2017-07-25 15:30:40 +0530

    Update Payload.java

--- src/main/java/config/Payload.java
+++ src/main/java/config/Payload.java
@@ -1,1 +1,30 @@
+package config;
 
+import java.util.List;
+
+public class Payload {
+
+	private String template_type;
+	public String getTemplate_type() {
+		return template_type;
+	}
+
+	public void setTemplate_type(String template_type) {
+		this.template_type = template_type;
+	}
+
+	public List<InnerButton> getButtons() {
+		return buttons;
+	}
+
+	public void setButtons(List<InnerButton> buttons) {
+		this.buttons = buttons;
+	}
+
+	private List<InnerButton> buttons;
+	
+	public Payload() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+}
