3e38bc6224625d84a18beb1c87e0a0e9fb410549
priyanka897 <priyanka.mittal@maxlifeinsurance.com>
2017-07-25 16:59:29 +0530

    Update Facebook.java

--- src/main/java/config/Facebook.java
+++ src/main/java/config/Facebook.java
@@ -5,14 +5,36 @@ import java.util.List;
 
 public class Facebook<Paylaod> 
 {
-	private String type;
-	private String platform;
-	private String title;
-	private String imageUrl;
+	//private String type;
+	//private String platform;
+	//private String title;
+	//private String imageUrl;
 	//private String payLoad;
 	//private List<InnerButton> buttons;
-	private List<Payload> payload;
-	private List<Payload> paylaod;
+	private List<Attachments> attachments;
+	public List<Attachments> getAttachments() {
+		return attachments;
+	}
+
+
+	public void setAttachments(List<Attachments> attachments) {
+		this.attachments = attachments;
+	}
+
+
+	public String getText() {
+		return text;
+	}
+
+
+	public void setText(String text) {
+		this.text = text;
+	}
+
+
+	private String text="";
+	//private List<Payload> payload;
+	//private List<Payload> paylaod;
 	
 	public Facebook() {
 		super();
@@ -20,17 +42,18 @@ public class Facebook<Paylaod>
 	}
 	
 
-	public Facebook(String type, String platform, String title, String imageUrl,  List<Paylaod> paylaod) {
+	//public Facebook(String type, String platform, String title, String imageUrl,  List<Paylaod> paylaod) {
+		public Facebook(List<Attachments> attachments, String text) {
 		super();
-		this.type = type;
-		this.platform = platform;
-		this.title = title;
-		this.imageUrl = imageUrl;
-		this.paylaod = payload;
+		this.attachments = attachments;
+		this.text = text;
+		//this.title = title;
+		//this.imageUrl = imageUrl;
+		//this.paylaod = payload;
 		//this.buttons = buttons;
 	}
 
-	public String getType() {
+	/*public String getType() {
 		return type;
 	}
 
@@ -71,12 +94,14 @@ public class Facebook<Paylaod>
 	public void setPayload(List<Payload> payload) {
 		this.payload = payload;
 	}
-	
+	*/
 	/*public List<InnerButton> getButtons() {
 		return buttons;
 	}
+
 	public void setButtons(List<InnerButton> buttons) {
 		this.buttons = buttons;
 	}*/
 
 }
+
