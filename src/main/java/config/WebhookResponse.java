1b46d4e8841d7d9e99ce5e031ab4fc79aac4cb58
priyanka897 <priyanka.mittal@maxlifeinsurance.com>
2017-07-25 15:25:39 +0530

    Update WebhookResponse.java

--- src/main/java/config/WebhookResponse.java
+++ src/main/java/config/WebhookResponse.java
@@ -6,14 +6,16 @@ public class WebhookResponse {
     private String speech;
     private String displayText;
     private List<ContextData> contextOut;
+    private InnerData data;
 
     private final String source = "java-webhook";
 
-	public WebhookResponse(String speech, String displayText, List<ContextData> contextOut) {
+	public WebhookResponse(String speech, String displayText, List<ContextData> contextOut, InnerData data) {
 		super();
 		this.speech = speech;
 		this.displayText = displayText;
 		this.contextOut = contextOut;
+		this.data = data;
 	}
 
     public String getSpeech() {
@@ -34,6 +36,13 @@ public class WebhookResponse {
 
 	public void setContextOut(List<ContextData> contextOut) {
 		this.contextOut = contextOut;
+	}
+	public InnerData getData() {
+		return data;
+	}
+
+	public void setData(InnerData data) {
+		this.data = data;
 	}
 	
 }
