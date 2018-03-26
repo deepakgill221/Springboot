b0b7686702ee1413f5ed27dbbf105262f45a95e8
manishnegi007 <32223363+manishnegi007@users.noreply.github.com>
2018-03-26 13:17:59 +0530

    Delete OTPService.java

--- src/main/java/cacheservice/OTPService.java
+++
@@ -1,9 +0,0 @@
-package cacheservice;
-
-import java.sql.SQLException;
-
-public interface OTPService 
-{
-	public String OTPCallCashing(String policyNo,String sessionId, String identifier, String finalDate) throws SQLException , Exception;
-
-}
