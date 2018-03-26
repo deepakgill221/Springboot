95b2fc1ef55facdf79a85b1ff888f05803b36276
manishnegi007 <32223363+manishnegi007@users.noreply.github.com>
2018-03-26 13:21:08 +0530

    Update ApplicationConfiguration.java

--- src/main/java/config/ApplicationConfiguration.java
+++ src/main/java/config/ApplicationConfiguration.java
@@ -2,16 +2,12 @@ package config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.ComponentScan;
 
-@EnableCaching
 @SpringBootApplication
-@ComponentScan({"handlerservice","config","commons","cacheservice"})
+@ComponentScan({"config"})
 public class ApplicationConfiguration {
-
 	public static void main(String[] args) {
 		SpringApplication.run(ApplicationConfiguration.class, args);
 	}
-
 }
